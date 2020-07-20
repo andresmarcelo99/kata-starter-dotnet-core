9617ba5fab3abb34641badfa30c264c73644eba8
Carlos Solorzano <csolorzanoflores@gmail.com>
2020-07-20 14:35:43 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,10 +1,16 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
     {
         public int Add(string numbers="")
         {
-            return 0;
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;                
+            }
+            return Int32.Parse(numbers);
         }
     }
 }
